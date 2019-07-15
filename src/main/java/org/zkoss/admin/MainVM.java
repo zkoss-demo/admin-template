7179c05d23a36513c8ec1c15e6ba39b41d0847c4
hawkchen <hawkchen@potix.com>
2019-07-15 16:02:53 +0800

    * add 2 VM and NavigationModel * create b-row

---
+++ src/main/java/org/zkoss/admin/MainVM.java
@@ -0,0 +1,24 @@
+package org.zkoss.admin;
+
+import org.zkoss.bind.annotation.*;
+import org.zkoss.zk.ui.Desktop;
+
+public class MainVM {
+
+    public static final String NAVIGATION = "navigation";
+    private NavigationMdel navigationModel;
+
+    @Init
+    public void init(@ContextParam(ContextType.DESKTOP) Desktop desktop){
+        navigationModel = new NavigationMdel();
+        desktop.setAttribute(NAVIGATION, navigationModel);
+    }
+
+    public String getContentUrl() {
+        return navigationModel.getContentUrl();
+    }
+
+    public NavigationMdel getNavigationModel() {
+        return navigationModel;
+    }
+}
---
+++ src/main/java/org/zkoss/admin/NavigationMdel.java
@@ -0,0 +1,13 @@
+package org.zkoss.admin;
+
+public class NavigationMdel {
+    private String contentUrl = "ecommerce/ecommerce.zul";
+
+    public String getContentUrl() {
+        return contentUrl;
+    }
+
+    public void setContentUrl(String contentUrl) {
+        this.contentUrl = contentUrl;
+    }
+}
