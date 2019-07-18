e73cfe7f3eb54c6bd267de73b650a68f018ad6dc
hawkchen <hawkchen@potix.com>
2019-07-18 18:46:18 +0800

    * define <counter> * use @MatchMedia for sidebar

--- src/main/java/org/zkoss/admin/SidebarVM.java
+++ src/main/java/org/zkoss/admin/SidebarVM.java
@@ -7,6 +7,9 @@ import static org.zkoss.admin.MainVM.NAVIGATION;
 
 public class SidebarVM {
     private NavigationMdel navigationModel;
+    private int counter[] = {2, 5};
+    private String userName = "Admin";
+    private String sidebarTemplate = "sidebar-md";
 
     @Init
     public void init(@ScopeParam(NAVIGATION)NavigationMdel navModel){
@@ -17,5 +20,29 @@ public class SidebarVM {
     public void navigate(@BindingParam("page")String page){
         navigationModel.setContentUrl(page);
         BindUtils.postNotifyChange(null, null, navigationModel, "contentUrl");
+    }
+
+    @MatchMedia("all and (min-width: 768px)")
+    @NotifyChange("sidebarTemplate")
+    public void beWide(){
+        sidebarTemplate = "sidebar-md";
+    }
+
+    @MatchMedia("all and (max-width: 767px)")
+    @NotifyChange("sidebarTemplate")
+    public void beNarrow(){
+        sidebarTemplate = "sidebar";
+    }
+
+    public int[] getCounter() {
+        return counter;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getSidebarTemplate() {
+        return sidebarTemplate;
     }
 }
