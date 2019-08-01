2dae214f5759bb3ee36344cba0d4b7cadc881d21
hawkchen <hawkchen@potix.com>
2019-08-01 13:00:14 +0800

    fix gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+// Kotlin DSL Primer https://docs.gradle.org/current/userguide/kotlin_dsl.html
 plugins {
     war
     id("org.gretty") version "2.3.1"
@@ -15,24 +16,20 @@ repositories {
     jcenter()
     maven {
         url = uri("http://mavensync.zkoss.org/eval")
-        content {
-            // this repository *only* contains artifacts with group specified
-            includeGroup("io.keikai")
-        }
     }
     maven {
-        url = uri("http://mavensync.zkoss.org/maven2")
-        content {
-            // this repository *only* contains artifacts with group specified
-            includeGroup("io.keikai")
-        }
-    }
+        url = uri("http://jaspersoft.jfrog.io/jaspersoft/third-party-ce-artifacts/")
+    }// for com.lowagie:itext:2.1.7.js6
 }
 
+val zkVer = "8.6.2-Eval"
+
 dependencies {
-    implementation("io.keikai:keikai-java-client:1.0.0-beta.20-Eval")
-    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
-    testCompile("junit", "junit", "4.12")
+    implementation("org.zkoss.zk:zkmax:${zkVer}")
+    implementation("org.zkoss.zk:zuti:${zkVer}")
+    implementation("org.zkoss.theme:iceblue_c:${zkVer}")
+    implementation("org.zkoss.chart:zkcharts:3.0.3-Eval")
+    implementation("org.webjars:bootstrap:4.3.1")
 }
 
 configure<JavaPluginConvention> {
