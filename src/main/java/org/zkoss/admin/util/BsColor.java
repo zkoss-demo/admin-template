832ad460499e16009a13afa385d46a1eb4969451
hawkchen <hawkchen@potix.com>
2019-08-01 12:16:20 +0800

    apply custom color code from the designer

--- src/main/java/org/zkoss/admin/util/BsColor.java
+++ src/main/java/org/zkoss/admin/util/BsColor.java
@@ -1,11 +1,11 @@
 package org.zkoss.admin.util;
 
 /**
- * bootstrap color code
+ * custom color code corresponding to bootstrap
  */
 public enum BsColor {
-    PRIMARY ("#007bff"), SUCCESS("#28a745"), WARNING("#ffc107")
-    , DANGER("#dc3545"), INFO("#17a2b8"), SECONDARY("#6c757d");
+    PRIMARY ("#0093F9"), SUCCESS("#15CAB4"), WARNING("#FFA516")
+    , DANGER("#FF4051"), INFO("#9665ED"), SECONDARY("#B2DEFD");
 
     private String hexCode;
 
